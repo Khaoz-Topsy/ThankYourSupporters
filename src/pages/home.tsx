b7df9802a60b86efb5c83291e3e17ed3cd4af16f
Kurt Lourens <hi@kurtlourens.com>
2023-09-05 21:20:54 +0200

    ✨ Add something to the homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,7 @@
 import type { Component } from 'solid-js';
 import { CommonLayout } from '../components/common/layout';
 import { PageHeader } from '../components/common/pageHeader';
-import { useNavigate } from '@solidjs/router';
+import { Link, useNavigate } from '@solidjs/router';
 import { routes } from '../constants/route';
 import { CenterLoading } from '../components/core/loading';
 
@@ -9,6 +9,17 @@ export const HomePage: Component = () => {
     return (
         <CommonLayout>
             <PageHeader text="Home"></PageHeader>
+
+            <div class="container mx-auto my-8 text-center noselect">
+                <img src="./assets/img/underConstruction.webp" class="mx-auto" alt="under construction" draggable="false" />
+
+                <p class="mt-4 text-xl max-w-lg mx-auto">This is a simple site to help you display your Patreon &amp; Youtube members in a simple way using the export functionality on their platforms.</p>
+
+                <Link href={routes.supporter}>
+                    <div class="btn btn-primary mt-8">Use it now</div>
+                </Link>
+
+            </div>
         </CommonLayout>
     );
 }
